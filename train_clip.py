019caa30f07009afb56c83b93ae44c4b568bf446
Octave Charrin <octave.charrin@gmail.com>
2023-05-26 14:40:09 +0200

    fix

--- train_clip.py
+++ train_clip.py
@@ -7,6 +7,7 @@ import torch.optim as optim
 from timm.data.auto_augment import rand_augment_transform
 from tqdm import tqdm
 from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
 from torchvision.transforms import transforms
 
 
@@ -29,19 +30,22 @@ def train(cfg):
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
                 transforms.ToTensor(),
             ])
+    rand_aug = rand_augment_transform(config_str='rand-m9-n3--mstd0.5',hparams={'img_mean': (0.485, 0.456, 0.406), 'img_std':(0.229, 0.224, 0.225)})
 
-    train_dataset = datamodule.train_dataset
-    class_to_idx = datamodule.dataset.class_to_idx
+    traindir = os.getcwd() + '/datasetV2/train_val'
+    valdir = os.getcwd() + '/datasetV2/val_train'
+
+    train_dataset = ImageFolder(traindir, transform=rand_aug)
+    val_dataset = ImageFolder(valdir, transform=simple_transform)
+    class_to_idx = train_dataset.class_to_idx
 
     class_list = list(range(48))
     for  (class_name, index) in class_to_idx.items():
         class_list[index] = class_name
     
-    rand_aug = rand_augment_transform(config_str='rand-m9-n3--mstd0.5',hparams={'img_mean': (0.485, 0.456, 0.406)})
-    train_dataset.transform.insert(0, rand_aug)
     
     train_loader = DataLoader(train_dataset, batch_size=datamodule.batch_size, shuffle=True, num_workers=datamodule.num_workers)
-    val_loader = datamodule.val_dataloader()
+    val_loader = DataLoader(val_dataset, batch_size=datamodule.batch_size, shuffle=False, num_workers=datamodule.num_workers)
 
     model, preprocess = clip.load("ViT-B/32", device=device)
     # model, preprocess = clip.load("ViT-B/16", device=device)
@@ -50,7 +54,7 @@ def train(cfg):
     # processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
     model.float()
-    
+
     for name, param in model.named_parameters():
         if name.startswith('transfomer'):
             param.requires_grad = False
@@ -99,7 +103,7 @@ def train(cfg):
         epoch_num_correct = 0
         num_samples = 0
 
-        path = os.getcwd() + f"/checkpoints/clip_training_checkpoint{epoch}.pt"
+        path = os.getcwd() + f"/checkpoints/freezeclip_training_checkpoint{epoch}.pt"
         torch.save(model.state_dict(), path)
 
         for _, batch in enumerate(val_loader):
